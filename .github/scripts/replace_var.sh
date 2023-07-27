1dd0eb1e16409d02c691ae001ee95e5527446111
yys3000 <51820144+yys3000@users.noreply.github.com>
2023-07-27 17:42:26 +0800

    Update replace_var.sh

--- .github/scripts/replace_var.sh
+++ .github/scripts/replace_var.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
-echo "hello world"
+values_file="values-DEV.yaml"
+cp $values_file ${values_file}.new
+json_file="vars.json"
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $MY_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/orgs/yangyys/actions/variables > $json_file
+
+echo $MY_TOKEN
+  
