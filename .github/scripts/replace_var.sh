7ee43c1b2075c7f65835323f506da29a262a7a11
yys3000 <51820144+yys3000@users.noreply.github.com>
2023-07-31 17:06:39 +0800

    Update replace_var-3.sh

--- .github/scripts/replace_var.sh
+++ .github/scripts/replace_var.sh
@@ -16,16 +16,14 @@ total_line=`cat $json_file | grep name | wc -l`
 for ((i=1; i<=$total_line; i++))
 do
     #name=`cat $json_file | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~"name"){print $(i+1)} }}' | tr -d '"' | sed -n ${i}p |sed 's/^[ ]*//g'`
-    name=`cat $json_file | awk -F\" '{for(i=1;i<=NF;i++){if($i~"name"){print $(i+2)} }}' | tr -d '"' | sed -n ${i}p |sed 's/^[ ]*//g'`
+    #name=`cat $json_file | awk -F\" '{for(i=1;i<=NF;i++){if($i~"name"){print $(i+2)} }}' | tr -d '"' | sed -n ${i}p |sed 's/^[ ]*//g'`
     #value=`cat $json_file | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~"value"){print $(i+1)} }}' | tr -d '"' | sed -n ${i}p |sed 's/^[ ]*//g'`
     #value=`cat $json_file | awk -F\" '{for(i=1;i<=NF;i++){if($i~"value"){print $(i+2)} }}' | tr -d '"' | sed -n ${i}p |sed 's/^[ ]*//g'`
+    name=`grep "\"name\":" $json_file |sed -n "${i}p" |awk -F\" '{print $4}'`
     value=`grep "\"value\":" $json_file |sed -n "${i}p" |awk -F\" '{print $4}'`
-    echo "<\$$name>"
-    echo $value
 
     if grep -q "<\$$name>" $values_file
     then
          sed -i "s|<\$$name>|$value|" ${values_file}.new
     fi
 done
-  
